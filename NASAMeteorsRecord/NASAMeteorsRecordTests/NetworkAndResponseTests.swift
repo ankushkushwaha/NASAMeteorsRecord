ed5b399f32a1fa6568765a3297971136886f0283
Ankush Kushwaha <ankush.kushwaha@starcut.com>
2021-07-17 12:50:53 +0300

    Fix class name typo

--- NASAMeteorsRecord/NASAMeteorsRecordTests/NetworkAndResponseTests.swift
+++ NASAMeteorsRecord/NASAMeteorsRecordTests/NetworkAndResponseTests.swift
@@ -1,5 +1,5 @@
 //
-//  NerworkAndResponseTests.swift
+//  NetworkAndResponseTests.swift
 //  NASAMeteorsRecordTests
 //
 //  Created by Ankush Kushwaha on 7/17/21.
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class NerworkAndResponseTests: XCTestCase {
+class NetworkAndResponseTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
