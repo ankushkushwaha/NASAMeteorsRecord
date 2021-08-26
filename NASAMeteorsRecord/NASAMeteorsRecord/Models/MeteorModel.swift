31034a54ff4d3468e65a3a31fb7774862a2c0478
Ankush Kushwaha <ankush.kushwaha@starcut.com>
2021-08-26 20:20:54 +0300

    Remove unused code

--- NASAMeteorsRecord/NASAMeteorsRecord/Models/MeteorModel.swift
+++ NASAMeteorsRecord/NASAMeteorsRecord/Models/MeteorModel.swift
@@ -10,21 +10,9 @@ import Foundation
 struct MeteorModel: Codable {
     let id: String
     let name: String?
-    let nametype: String?
-    let recclass: String?
     let mass: String?
-    let fall: Fall?
     let year: Date?
-    let reclat: String?
-    let reclong: String?
     let geolocation: Geolocation?
-    let computedRegionCbhkFwbd: String?
-    let computedRegionNnqa25F4: String?
-}
-
-enum Fall: String, Codable {
-    case fell = "Fell"
-    case found = "Found"
 }
 
 struct Geolocation: Codable {
